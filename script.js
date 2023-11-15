bf35bcd355f0a00c05e020c5a29c4b46f8377d16
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-16 00:42:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const makeChange = (c) => {
   // your name here
+	let change={
+		q: "",
+		d: "",
+		n: "",
+		c: ""
+	};
+	let coinValue={
+		q: "25",
+		d: "10",
+		n: "5",
+		c: "1"
+	};
+
+	for(let coin in coinValue) {
+		change[coin] = Math.floor(c / parseInt(coinValue[coin]));		
+		c = Math.floor(c % parseInt(coinValue[coin]));
+	}
+
+	return change;
+	
 };
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
+
 alert(JSON.stringify(makeChange(c)));
